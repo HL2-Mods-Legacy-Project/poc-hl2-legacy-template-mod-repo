cac7b8e889fee4ec75b70669a7e9233ffed7a1fa
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-06-29 10:48:36 -0400

    Create build_all.ps1

---
+++ scripts/build_all.ps1
@@ -0,0 +1,8 @@
+& "$PSScriptRoot\..\mod-installer\build_all.ps1" `
+  -Version "<version>" `
+  -OutputFilePrefix "<output file prefix>" `
+  -ModName "<mod name>" `
+  -ModFilesPath "$PSScriptRoot\..\game\<mod folder>" `
+  -ModBaseFilesUrl "<url to mod base files zip>" `
+  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
+  -PublishPath "$PSScriptRoot\..\artifacts\publish"
